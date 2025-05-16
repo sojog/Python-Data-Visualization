3fe6abd024b51ae7c641d6c642444da39abf8b87
Silviu Ojog <sil.ojog@gmail.com>
2025-05-16 19:14:32 +0300

    Categorii

--- Curs3/Ecommerce/shop/urls.py
+++ Curs3/Ecommerce/shop/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import category_details_view
+from .views import all_categories_view
 from .views import product_details_view
 from .views import all_products_view
 
@@ -6,4 +8,6 @@ urlpatterns = [
 
 	path("all", all_products_view),
 	path("details", product_details_view),
+	path("categories", all_categories_view),
+	path("category", category_details_view),
 ]
--- Curs3/Ecommerce/shop/views.py
+++ Curs3/Ecommerce/shop/views.py
@@ -16,3 +16,11 @@ def all_products_view(request):
 def product_details_view(request):
 	context = {}
 	return render(request, 'product_details.html', context)
+
+def all_categories_view(request):
+	context = {}
+	return render(request, 'all_categories.html', context)
+
+def category_details_view(request):
+	context = {}
+	return render(request, 'category_details.html', context)
