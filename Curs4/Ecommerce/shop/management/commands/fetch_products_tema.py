b7028482653ac57ce7b088856130967f9af86b63
Silviu Ojog <sil.ojog@gmail.com>
2025-05-21 18:30:44 +0300

    Tema

--- Curs4/Ecommerce/shop/management/commands/fetch_products_tema.py
+++ Curs4/Ecommerce/shop/management/commands/fetch_products_tema.py
@@ -4,3 +4,34 @@
 ##         (nume, pret, imagine...)
 
 ## Luati 10 produse si le pune in DB
+
+
+from django.core.management.base import BaseCommand, CommandError
+from shop.models import Product
+import requests
+
+### python manage.py fetch_product
+class Command(BaseCommand):
+    help = "Fetch product from https://dummyjson.com/products/product"
+
+    def handle(self, *args, **options):
+        BASE_URL = "https://dummyjson.com/"
+        URL_PRODUCTS = BASE_URL + "products"
+
+        LIMIT = 1
+
+        response = requests.get(URL_PRODUCTS, {"limit":LIMIT})
+        product_list = response.json()["products"]
+
+        for prod_dict in product_list:
+
+            name = prod_dict["title"]
+            slug = name.lower().replace(" ", "-")
+            description = prod_dict["description"] 
+            
+            price = prod_dict["price"]
+
+
+            Product.objects.create(name=name, slug=slug, description=description, price=price)
+
+        print("S-a terminat...")
