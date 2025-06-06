34a999bf562e5cbd6cb1480cee41f678cb6a95a6
Silviu Ojog <sil.ojog@gmail.com>
2025-06-06 19:36:11 +0300

    Logica de cart

--- Curs7/Ecommerce/cart/views.py
+++ Curs7/Ecommerce/cart/views.py
@@ -1,26 +1,95 @@
 from django.shortcuts import render, redirect
-
+from  shop.models import Product 
 # Create your views here.
 
-class Cart:
+class CartLogic:
 	def __init__(self, request):
 		self.request = request
+
+		##  { "SLUG" : quantity }
+		self.produse_existente = request.session.get("cos", { })
+
+	### Totalul cartului
+
+	@property
+	def total_price(self):
+		return sum( [product.price * quantity  for product, quantity in self.products.items()] )
+
+
+	@property
+	def number_of_products(self):
+		return sum(self.produse_existente.values())
+
+
+	### Produsele din cart si numarul lor
+
+
+	@property
+	def products_mai_putin_eficienta(self):
+		product_slugs = self.produse_existente.keys()
+
+		real_products = {  }
+		for slug in product_slugs:
+			produsul_efectiv = Product.objects.filter(slug=slug).first()
+			real_products[produsul_efectiv] = self.produse_existente[slug]
+
+		return real_products
+	
+
+
+	@property
+	def products(self):
+		product_slugs = self.produse_existente.keys()
+
+		products_with_slugs = Product.objects.filter(slug__in=product_slugs)
+		real_products = {  }
+
+		for product in products_with_slugs:
+			real_products[product] = self.produse_existente[product.slug]
+
+		return real_products
+
+	### Adaugare produs in cart
+	def add(self, slug, quantity):
+
+		existing_quantity = self.produse_existente.get(slug, 0)
+		quantity += existing_quantity
+
+		self.produse_existente[slug] =  quantity
+		self.request.session["cos"] = self.produse_existente
+
+	### Stergere produs din cart
+	def remove(self, product):
+		pass
+	
+	### Stergere cart
+	def clear(self):
+		pass
+
+
+
+
+
+
+
 
 
 def cart_view(request):
 
-	## TODO:  cart = Cart(request)
-	## cart.exising_products
+	# TODO: 
+	cart_logic = CartLogic(request)
+	context = { 
+			"cart":cart_logic
+			
+			}
 
-	produse_existente = request.session.get("cos", { })
-	context = {"produse":produse_existente}
+
 	return render(request, 'cart.html', context)
 
+
+
+
 def add_to_cart_view(request, slug):
-
-
-	## TODO:  cart = Cart(request)
-
 
 
 	print("Ar trebui adaugat in cos..", slug)
@@ -29,15 +98,13 @@ def add_to_cart_view(request, slug):
 	print("Sesiune", request.POST)
 
 
-	produse_existente = request.session.get("cos", { })
-	
+
+	cart_logic = CartLogic(request)
 	quantity = request.POST.get("quantity", 1)
 	quantity = int(quantity)
-	existing_quantity = produse_existente.get(slug, 0)
-	quantity += existing_quantity
-	produse_existente[slug] =  quantity
-	request.session["cos"] = produse_existente
 
+	cart_logic.add(slug, quantity)
+	
 
 
 	return redirect("cart_url")
--- Curs7/Ecommerce/shop/tests.py
+++ Curs7/Ecommerce/shop/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+d = { "hello":"world"}
+
+for value, key in enumerate(d):
+    print(value, key)
